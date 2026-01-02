4512ec16ff95ef6c992faab605013fbd91f3c01c
Fabrice Boyer <3055419+FabriceBoyer@users.noreply.github.com>
2026-01-02 17:59:51 +0100

    download

--- download_data.sh
+++ download_data.sh
@@ -3,9 +3,13 @@
 # Common utils
 wget_cmd="wget -q --show-progress --limit-rate=1000M"
 
-source $(dirname -- "$0")/.env
+current_dir=$(dirname "$0")
+source $current_dir/.env
 out_dir=$DUMP_PATH
 mkdir -p $out_dir
 
 # Crossref Metadata
-# Download the metadata via torrent crossref_2023.torrent in folder $DUMP_PATH
+# Download the metadata via torrent crossref_2025.torrent in folder $DUMP_PATH
+# sudo apt install transmission -y
+transmission-cli -w $out_dir $current_dir/crossref_2025.torrent
+
